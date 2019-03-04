b0a8cedcafff5eddf5f7666705dab012cd5efe0b
PadmanabhanWebD <padmanabhan.developer.email@gmail.com>
2019-03-04 10:14:36 +0530

    change bureau click event process on list page

--- admin/src/updateprofilestatus.php
+++ admin/src/updateprofilestatus.php
@@ -16,5 +16,17 @@ if($update_value == 1 && $profile_number != ''){
     $profile_update = $db->prepare($membership_update_query);
     $profile_update->execute();
 }
+if($update_value == 5 && $profile_id != ''){
+    $get_current_profile_number = "SELECT profile_number from memberships where profile_id = '".$profile_id."'";
+    $current_pn = $db->prepare($get_current_profile_number);
+    $current_pn->execute();
+    $current_profile_number = $current_pn->fetch(0)['profile_number'];
+    $new_profile_number = 'B'.substr($current_profile_number, 1);
+    
+    $profile_number_update_bureau = "UPDATE memberships set profile_number = '".$new_profile_number."' where profile_id = '".$profile_id."'";
+    $profile_number_update_bureau_query = $db->prepare($profile_number_update_bureau);
+    $profile_number_update_bureau_query->execute();
+    
+}
 
-echo "success";
+    echo "success";
