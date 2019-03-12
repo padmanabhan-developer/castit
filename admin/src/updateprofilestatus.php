5c67888192da28b3e773f67df3bc36e2edac7157
PadmanabhanWebD <padmanabhan.developer.email@gmail.com>
2019-03-12 16:42:25 +0530

    12 Mar deployment

--- admin/src/updateprofilestatus.php
+++ admin/src/updateprofilestatus.php
@@ -11,22 +11,30 @@ $profile_update_query = "UPDATE profiles SET profile_status_id = ".$update_value
 $profile_update = $db->prepare($profile_update_query);
 $profile_update->execute();
 
+$get_current_profile_number = "SELECT profile_number from memberships where profile_id = '".$profile_id."'";
+$current_pn = $db->prepare($get_current_profile_number);
+$current_pn->execute();
+$current_profile_number = $current_pn->fetch(0)['profile_number'];
+$current_prefix = substr($current_profile_number, 0, 1);
+
 if($update_value == 1 && $profile_number != ''){
     $membership_update_query = "UPDATE memberships set current = 1 where profile_id = '".$profile_id."' and profile_number = '". $profile_number."'" ;
     $profile_update = $db->prepare($membership_update_query);
     $profile_update->execute();
 }
-if($update_value == 5 && $profile_id != ''){
-    $get_current_profile_number = "SELECT profile_number from memberships where profile_id = '".$profile_id."'";
-    $current_pn = $db->prepare($get_current_profile_number);
-    $current_pn->execute();
-    $current_profile_number = $current_pn->fetch(0)['profile_number'];
-    $new_profile_number = 'B'.substr($current_profile_number, 1);
-    
+
+if($update_value != 5 && $current_prefix == 'B'){
+    $new_profile_number = 'Y'.substr($current_profile_number, 1);
     $profile_number_update_bureau = "UPDATE memberships set profile_number = '".$new_profile_number."' where profile_id = '".$profile_id."'";
     $profile_number_update_bureau_query = $db->prepare($profile_number_update_bureau);
     $profile_number_update_bureau_query->execute();
-    
 }
 
-    echo "success";
+if($update_value == 5 && $profile_id != ''){
+    $new_profile_number = 'B'.substr($current_profile_number, 1);
+    $profile_number_update_bureau = "UPDATE memberships set profile_number = '".$new_profile_number."' where profile_id = '".$profile_id."'";
+    $profile_number_update_bureau_query = $db->prepare($profile_number_update_bureau);
+    $profile_number_update_bureau_query->execute();
+}
+
+echo "success";
